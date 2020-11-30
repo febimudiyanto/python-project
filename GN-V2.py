6dc2ec11fedd32ecea63a538341d855de2dd1845
febimudiyanto <mudiyantofebi@gmail.com>
2020-11-30 14:35:43 +0700

    add error handling

--- GN-V2.py
+++ GN-V2.py
@@ -5,7 +5,7 @@
 import random
 
 # angka rahasia
-rahasia = random.randrange(1,100)
+rahasia = random.randint(1,100)
 
 # kesempatan
 K = 7
@@ -15,26 +15,32 @@ print("gunakan sebaik-baiknya, tebak dengan tepat")
 print("--------------------------------------------")
 
 while K > 0:
-    # user menginput suatu angka
-    print("\n------------------------sisa nyawa:",K)
-    tebakan =int(input("tebak sebuah angka = "))
-
-    # pengondisian
-    if tebakan == rahasia:
-        print("----------------------------------------------")
-        print("|\n|\t Selamat..... anda berhasil menebak!")
-        print("|")
-        print("----------------------------------------------")
-        #loopnya akan selesai
-        break
-    elif tebakan > rahasia:
-        print("angka terlalu tinggi!")
-    elif tebakan < rahasia:
-        print("angka terlalu rendah!")
-    
-    # kesempatan berkurang
-    K -= 1
-
+    # erorr handling
+    try:
+        # user menginput suatu angka
+        print("\n------------------------sisa nyawa:",K)
+        
+        tebakan =int(input("tebak sebuah angka = "))
+        
+        # pengondisian
+        if tebakan == rahasia:
+            print("----------------------------------------------")
+            print("|\n|\t Selamat..... anda berhasil menebak!")
+            print("|")
+            print("----------------------------------------------")
+            #loopnya akan selesai
+            break
+        elif tebakan > rahasia:
+            print("angka terlalu tinggi!")
+        elif tebakan < rahasia:
+            print("angka terlalu rendah!")
+        
+        # kesempatan berkurang
+        K -= 1
+        
+    except ValueError:
+        print("\n!!! Input kamu salah")
+        print("!!! hanya boleh bilangan integer")
 
 #jika loopnya selesai
 if K <= 0:
