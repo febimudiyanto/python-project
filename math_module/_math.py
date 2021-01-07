b5afec4105a32e7a6e6e290480f4ec3a300dfa64
Febi Mudiyanto <62255283+febimudiyanto@users.noreply.github.com>
2021-01-07 15:23:10 +0700

    Create _math.py

---
+++ math_module/_math.py
@@ -0,0 +1,26 @@
+def cek_prima(angka): 
+    # angka = 5
+    #inisialisasi awal
+    prima = True
+    # filter untuk cek prima
+    if angka < 2:
+      prima = False # bukan prima
+    else:
+      for x in range(2,angka): 
+        if (angka % x == 0):
+          prima = False # bukan prima
+          break
+
+    if prima:
+      print("bilangan prima")
+    else:
+      print("bukan prima")
+
+
+def cek_genap(angka):
+  if (angka%2==0):
+    print("angka genap")
+  else:
+    print('angka ganjil')
+
+
