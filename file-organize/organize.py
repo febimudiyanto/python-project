3ffd48f05c676f142a6d0e4ff2bf6a4930608de2
febimudiyanto <mudiyantofebi@gmail.com>
2021-04-22 23:57:25 +0700

    file automation

---
+++ file-organize/organize.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+
+SUBDIR = {
+        "DOCUMENTS":[".pdf",".docx",".txt"],
+        "AUDIO":[".m4a",".m4b",".mp3"],
+        "IMAGES":[".jpg",".jpeg",".png"]
+        }
+def pickDir(value):
+    for category, ekstensi in SUBDIR.items():
+        for suffix in ekstensi:
+            if suffix == value:
+                return category
+
+def organizeDir():
+    for item in os.scandir():
+        if item.is_dir():
+                continue
+        filePath = Path(item)
+        filetype = filePath.suffix.lower()
+        directory = pickDir(filetype)
+        directoryPath = Path(directory)
+        if directoryPath.is_dir() != True:
+                directoryPath.mkdir()
+        filePath.rename(directoryPath.joinpath(filePath))
+organizeDir()
