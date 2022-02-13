afc3d85ccd24d929ab219787a9b97774379f940b
Febi Mudiyanto <62255283+febimudiyanto@users.noreply.github.com>
2022-02-13 22:01:57 +0700

    Create genKey.py

---
+++ encryption/genKey.py
@@ -0,0 +1,21 @@
+'''
+pip install pycryptodome
+'''
+
+from Crypto.PublicKey import RSA
+
+key = RSA.generate(2048)
+privateKey = key.export_key()
+publicKey = key.publickey().export_key()
+
+# save private key to file
+with open('private.pem', 'wb') as f:
+    f.write(privateKey)
+
+# save public key to file
+with open('public.pem', 'wb') as f:
+    f.write(publicKey)
+
+print('Private key saved to private.pem')
+print('Public key saved to public.pem')
+print('Done')
