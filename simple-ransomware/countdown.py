e9784a0cbe92116633d8c16f79ce468016843f4c
Febi Mudiyanto <62255283+febimudiyanto@users.noreply.github.com>
2022-02-27 00:15:31 +0700

    Add files via upload

---
+++ simple-ransomware/countdown.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+
+def countdown(count):
+    # change text in label
+    # count = '01:30:00'
+    hour, minute, second = count.split(':')
+    hour = int(hour)
+    minute = int(minute)
+    second = int(second)
+
+    label['text'] = '{}:{}:{}'.format(hour, minute, second)
+
+    if second > 0 or minute > 0 or hour > 0:
+        # call countdown again after 1000ms (1s)
+        if second > 0:
+            second -= 1
+        elif minute > 0:
+            minute -= 1
+            second = 59
+        elif hour > 0:
+            hour -= 1
+            minute = 59
+            second = 59
+        root.after(1000, countdown, '{}:{}:{}'.format(hour, minute, second)) 
+
+root = tk.Tk()
+root.title('L0v3sh3 Ransomware')
+root.geometry('500x300')
+root.resizable(False, False)
+label1 = tk.Label(root, text='Your data is under rest, please don\'t pay me,\nthis just simulation !!\n\n', font=('calibri', 12,'bold'))
+label1.pack()
+label = tk.Label(root,font=('calibri', 50,'bold'), fg='white', bg='blue')
+label.pack()
+
+# call countdown first time    
+countdown('01:30:00')
+# root.after(0, countdown, 5)
+root.mainloop()
